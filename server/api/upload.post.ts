26b952de69916824348ea94689488f61edd18b00
bertyn99 <bboulikou@gmail.com>
2025-02-21 16:08:21 +0100

    local storage of the  file for the demo

--- server/api/upload.post.ts
+++ server/api/upload.post.ts
@@ -2,15 +2,36 @@ import { defineEventHandler, readBody } from 'h3'
 import { pdfQueue } from '../utils/queue'
 
 export default defineEventHandler(async (event) => {
-  const formData = await readFormData(event)
-  const file = formData.get('file')
-  
-  // Add job to BullMQ queue
-  const job = await pdfQueue.add('process-pdf', {
-    file: file, // You might want to save the file somewhere first
-  })
-  
-  return {
-    jobId: job.id
+  try {
+    const formData = await readFormData(event)
+    const file = formData.get('file') as File
+    
+    //store the file locally  or in S3 compatible api with adequate driver
+    const storage = useStorage('local')
+    
+    // Convert file to buffer
+    const buffer = Buffer.from(await file.arrayBuffer())
+
+    const formatedName=formatFileName(file.name)
+
+    await storage.setItemRaw(formatedName,buffer)
+
+    // Add job to BullMQ queue
+    const job = await pdfQueue.add('process-pdf', {
+      file: {
+        name:formatedName,
+        path:`./.data/file/${formatedName}`,
+        option:{} 
+
+      },
+    })
+    
+    return {
+      jobId: job.id
+    } 
+  } catch (error:any) {
+    console.error(error)
+    throw createError({statusMessage:error.msg,statusCode:500})
   }
+  
 })
